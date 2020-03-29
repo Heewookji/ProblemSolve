5f8418b83dd6a8f202e9540635e63e353859680d
heewookji <wlgmldnr@daum.net>
2020-03-29 17:21:20 +0900

    boggle5

--- src/main/java/Boggle/Main.java
+++ src/main/java/Boggle/Main.java
@@ -1,7 +1,6 @@
 package Boggle;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -9,7 +8,7 @@ public class Main {
   static int[] dx = {1,1,0,-1,-1,-1,0,1};
   static int[] dy = {0,1,1,1,0,-1,-1,-1};
   static char[][] boggle = new char[5][5];
-  static HashMap<String, Boolean> cache = null; 
+  static int[][][] cache = null; 
 
 
   public static void main(String[] args) throws IOException {
@@ -21,7 +20,6 @@ public class Main {
 
     for(int i=0; i<caseNum; i++) {
 
-      cache = new HashMap<String,Boolean>();
       for(int y=0; y<5; y++) {
         boggle[y] = keyboard.nextLine().toCharArray();
       }
@@ -35,10 +33,13 @@ public class Main {
       }
 
       for(int j=0;j<wordNum;j++) {
+
+        cache = new int[5][5][keywords[j].length()];
         boolean flag = false;
+
         find: for(int y=0; y<5; y++) {
           for(int x=0; x<5; x++) {
-            if(find(x,y,keywords[j])) {
+            if(find(x,y,keywords[j],0) == 1) {
               flag = true;
               break find;
             }
@@ -51,31 +52,23 @@ public class Main {
     keyboard.close();
   }
 
-  private static boolean find(int x, int y, String keyword) {
+  private static int find(int x, int y, String keyword, int index) {
 
-    if(x>4||x<0||y>4||y<0) return false;
-    if(boggle[x][y] != keyword.charAt(0)) return false;
-    if(keyword.length() == 1) return true;
-
+    if(x>4||x<0||y>4||y<0) return -1;
+    if(boggle[x][y] != keyword.charAt(0)) return cache[x][y][index] = -1;
+    if(cache[x][y][index] != 0) return cache[x][y][index];
+    if(keyword.length() == 1) return cache[x][y][index] = 1;
+    
     for(int eightWay=0;eightWay<8;eightWay++) {
       int nextX = x+dx[eightWay];
       int nextY = y+dy[eightWay];
       String nextKeyword = keyword.substring(1);
-      Boolean cacheFlag = cache.get(nextX+","+nextY+","+nextKeyword);
 
-      if(cacheFlag != null && cacheFlag) {
-        return true;
-      } else if(cacheFlag != null && !cacheFlag) {
-        return false;
-      }
-
-      if(find(nextX,nextY,nextKeyword)) {
-        cache.put(nextX+","+nextY+","+nextKeyword, true );
-        return true;
+      if(find(nextX,nextY,nextKeyword, index+1) == 1) {
+        return cache[x][y][index] = 1;
       }
     }
-    cache.put(x+","+y+","+keyword, false);
-    return false;
+    return cache[x][y][index] = -1;
   }
 
 }
