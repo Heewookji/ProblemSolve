a60cce9fafe76c7346106b4de02759abb67700ef
Heewookjigit <wlgmldnr@daum.net>
2020-04-01 17:57:26 +0900

    --picnic

--- src/main/java/picnic/Main.java
+++ src/main/java/picnic/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
   static int stuN;
-  static int[][] couples;
+  static boolean[][] couples;
 
 
   public static void main(String[] args) {
@@ -18,38 +18,38 @@ public class Main {
 
       stuN = sc.nextInt();
       int coupleN = sc.nextInt();
-      couples = new int[coupleN][2];
+      couples = new boolean[stuN][stuN];
 
       for(int cn=0; cn<coupleN; cn++) {
-        couples[cn][0] = sc.nextInt();
-        couples[cn][1] = sc.nextInt();
+        couples[sc.nextInt()][sc.nextInt()] = true;
       }
-      
-      int sum = 0;
-      for(int i=0; i<couples.length; i++) {
-        sum += find(i, new boolean[10], 0);
-      }
+
+      int sum = find(new boolean[stuN]);
       System.out.println(sum);
     }
     sc.close();
   }
 
-  private static int find(int index, boolean[] used, int count) {
+  private static int find(boolean[] students) {
 
-    if(used[couples[index][0]] || used[couples[index][1]]) return 0;
-    count = count + 2;
-    if(count == stuN) return 1;
+    int first = -1;
+    for(int i=0; i<students.length; i++) {
+      if(!students[i]) {
+        first = i;
+        break;
+      }
+    }
+    if(first == -1) return 1;
 
     int sum = 0;
-    for(int i=0; i<couples.length; i++) {
-      used[couples[index][0]] = true;
-      used[couples[index][1]] = true;
-      sum += find(i, used, count);
-      used[couples[index][0]] = false;
-      used[couples[index][1]] = false;
+    for(int pair=first+1; pair<stuN; pair++) {
+      if(!students[pair] && (couples[first][pair]||couples[pair][first])) {
+        students[first] = students[pair] = true;
+        sum += find(students);
+        students[first] = students[pair] = false;
+      }
     }
     return sum;
   }
-
 
 }
