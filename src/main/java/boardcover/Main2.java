56938a42c823c13d3b4397f7354874034a284db5
heewookji <wlgmldnr@daum.net>
2020-04-06 22:36:40 +0900

    boardcover

--- src/main/java/boardcover/Main2.java
+++ src/main/java/boardcover/Main2.java
@@ -8,10 +8,10 @@ public class Main2 {
   static int yN;
   static int xN;
   static int[][][] pos = {
-      {{0,-1},{-1,-1}},
-      {{0,-1},{1,-1}},
-      {{1,0},{0,-1}},
-      {{1,0},{1,-1}}
+      {{0,0},{0,1},{-1,1}},
+      {{0,0},{0,1},{1,1}},
+      {{0,0},{1,0},{0,1}},
+      {{0,0},{1,0},{1,1}}
   };
 
   public static void main(String[] args) {
@@ -40,7 +40,6 @@ public class Main2 {
     sc.close();
   }
 
-
   private static int find() {
 
     int x = -1;
@@ -60,10 +59,9 @@ public class Main2 {
     int sum = 0;
     for(int i=0; i<4; i++) {
       if(next(i,x,y,1)) {
-        //true면 채우고 재귀호출
         sum += find();
-        next(i,x,y,-1);
       }
+      next(i,x,y,-1);
     }
     return sum;
   }
@@ -71,7 +69,7 @@ public class Main2 {
   private static boolean next(int i, int x, int y, int opt) {
 
     boolean flag = true;
-    for(int count=0; count<2; count++) {
+    for(int count=0; count<3; count++) {
       int nextX = x + pos[i][count][0];
       int nextY = y + pos[i][count][1];
       if(nextX < 0 || nextX >= xN || nextY < 0 || nextY >= yN) {
@@ -81,8 +79,6 @@ public class Main2 {
     }
     return flag;
   }
-
- 
 }
 
 /**
