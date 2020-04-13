19b926729e338bcdf62681552bb12a7737ad762c
heewookji <wlgmldnr@daum.net>
2020-04-13 22:35:52 +0900

    fence

--- src/main/java/fence/Main.java
+++ src/main/java/fence/Main.java
@@ -13,9 +13,24 @@ public class Main {
     
     for(int t=0; t<testN; t++) {
       
+      int woodN = sc.nextInt();
+      int[] woods = new int[woodN];
       
+      for(int w=0; w<woodN; w++){
+        woods[w] = sc.nextInt();
+      }
+      
+      int ret = 0;
+      for(int i=0; i<woodN; i++) {
+        int least = 10000;
+        for(int j=i; j<woodN; j++) {
+          if(least > woods[j]) least = woods[j];
+          if( least * (j-i+1) > ret ) ret = least * (j-i+1);
+        }
+      }
+      System.out.println(ret);
     }
-    
+    sc.close();
   }
   
   
