636b4adfb7884d3ff5ae3cf828f08a1fd43bfb38
Heewookjigit <wlgmldnr@daum.net>
2020-04-13 17:51:53 +0900

    quadtree

---
+++ src/main/java/fence/Main.java
@@ -0,0 +1,22 @@
+package fence;
+
+import java.util.Scanner;
+
+public class Main {
+
+  
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    int testN = sc.nextInt();
+    
+    for(int t=0; t<testN; t++) {
+      
+      
+    }
+    
+  }
+  
+  
+}
--- src/main/java/quadtree/Main.java
+++ src/main/java/quadtree/Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Main {
 
-  static String[][] board;
-  
+  static int point;
+
   public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
@@ -13,27 +13,28 @@ public class Main {
     int testN = sc.nextInt();
     sc.nextLine();
 
-    for(int t=0; t<testN; t++) {
+    for(int t=0; t< testN; t++) {
       String line = sc.nextLine();
-      board = new String[16][16];
-      draw(line);
-
-      for(int i=0; i<16; i++) {
-        for(int j=0; j<16; j++) {
-          System.out.print(board[i][j]);
-        }
-        System.out.println();
-      }
+      point = 0;
+      String ret = reverse(line);
+      System.out.println(ret);
     }
-  }
-  
-  private static void draw(String line) {
-    
-    char head = line.charAt(0);
-    if(head == 'w' || head == 'b' ) {
-      
-    }
-    
+    sc.close();
   }
 
+  public static String reverse(String line) {
+    char head = line.charAt(point);
+    point++;
+    if(head == 'b' || head == 'w') {
+      return String.valueOf(head);
+    }
+    else {
+      String upperLeft = reverse(line);
+      String upperRight = reverse(line);
+      String lowerLeft = reverse(line);
+      String lowerRight = reverse(line);
+
+      return "x" + lowerLeft + lowerRight + upperLeft + upperRight; 
+    }
+  }
 }
