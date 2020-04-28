dc55c926cf0548b50707e17466164b4fa5798273
Heewookjigit <wlgmldnr@daum.net>
2020-04-28 13:57:40 +0900

    poly

--- src/main/java/ㅡ20poly/Main.java
+++ src/main/java/ㅡ20poly/Main.java
@@ -4,9 +4,8 @@ import java.util.Scanner;
 
 public class Main {
 
-  static int[][] board;
-  static int n;
-  static int[][] pos = {{0,1},{1,0},{0,-1},{-1,0}};
+  static int[][] cache;
+  static int MOD = 10000000;
 
   public static void main(String[] args) {
 
@@ -15,47 +14,26 @@ public class Main {
 
     for(int t=0; t<testN; t++) {
 
-      n = sc.nextInt();
-      board = new int[n][n];
-
-      System.out.println( find(n, 0, 0));
+      int n = sc.nextInt();
+      cache = new int[n+1][n+1];
+      int sum = 0;
+      for(int i=1; i<=n; i++) {
+        sum = (sum + find(n, i))%MOD;
+      }
+      System.out.println(sum);
     }
   }
 
-  private static int find(int n, int x, int y) {
+  private static int find(int n, int first) {
 
-    if(!isMono()) return 0;
-    if(n == 0) return 1;
-
+    if(n == first) return 1;
+    if(cache[n][first] != 0) return cache[n][first];
     int sum = 0;
-    for(int i=0; i<4; i++) {
-      int nextX = x + pos[i][0];
-      int nextY = y + pos[i][1];
-      if(!isFilled(nextX,nextY, 1)) {
-        sum += find(n-1, nextX, nextY);
-      }
-      isFilled(nextX, nextY, -1);
+    for(int second=1; second<=n-first; second++) {
+      sum = (sum + (first+second-1) * find(n-first, second))%MOD;
     }
-    return sum;
+    return cache[n][first] = sum;
   }
 
-  private static boolean isMono() {
-    for(int y=0; y<n; y++) {
-      boolean flag1 = false;
-      boolean flag2 = false;
-      for(int x=0; x<n; x++) {
-        if(board[y][x] == 1) flag1 = true;
-        if(flag1 && board[y][x] == 0) flag2 = true;
-        if(flag2 && board[y][x] == 1) return false;
-      }
-    }
-    return true;
-  }
-
-  private static boolean isFilled(int x, int y, int flag) {
-    if(x<0 || x>=n || y<0 || y>=n) return false;
-    if((board[y][x] += flag) > 1 ) return false;
-    return true;
-  }
 
 }
