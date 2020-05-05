1c67f3e22d6676ac8add0784dd073fcb042a5ca6
heewookji <wlgmldnr@daum.net>
2020-05-05 22:51:22 +0900

    quadtree

---
+++ src/main/java/ㅡ06quadtree/Main2.java
@@ -0,0 +1,24 @@
+package ㅡ06quadtree;
+
+import java.util.Scanner;
+
+public class Main2 {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    int testN = sc.nextInt();
+    sc.nextLine();
+
+    for(int t=0; t<testN; t++) {
+      String line = sc.nextLine();
+      System.out.println(find(line));
+    }
+  }
+
+  private static String find(String line) {
+    return null;  
+  }
+
+}
