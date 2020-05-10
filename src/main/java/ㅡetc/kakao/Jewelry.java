219f48d75fa66a8799dc773b72250f560357c12f
heewookji <wlgmldnr@daum.net>
2020-05-10 19:06:01 +0900

    jewelry

--- src/main/java/ㅡetc/kakao/Jewelry.java
+++ src/main/java/ㅡetc/kakao/Jewelry.java
@@ -6,24 +6,30 @@ public class Jewelry {
 
   public static void main(String[] args) {
 
-    //String[] gems = {"","DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
-    String[] gems = {"AA", "AB", "AC", "AA", "AC"};
-    //String[] gems = {"XYZ", "XYZ", "XYZ"};
-    //String[] gems = {"ZZZ", "YYY", "NNNN", "YYY", "BBB"};
-    int[] ret = solution(gems);
+    String[] gems1 = {"","DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
+    String[] gems2 = {"","AA", "AB", "AC", "AA", "AC"};
+    String[] gems3 = {"","XYZ", "XYZ", "XYZ"};
+    String[] gems4 = {"","ZZZ", "YYY", "NNNN", "YYY", "BBB"};
+    int[] ret = solution(gems4);
     System.out.println(ret[0] + "," + ret[1]);
   }
 
+  static int least;
+
   public static int[] solution(String[] gems) {
     int[] answer = {-1,-1};
-    int least = init(gems);
+    least = init(gems);
     System.out.println(least);
-    
+
+    int ans = gems.length;
     for(int pos=1; pos<gems.length; pos++) {
       for(int len=pos; len<gems.length; len++) {
+        boolean flag = false;
         int ret = isCorrect(pos,len, gems, least);
-        if(ret != -1 && least >= ret) {
-          least = ret;
+        if(ans > ret) flag = true;
+        else if(ans == ret && answer[0] > pos) flag = true;
+        if(flag) {
+          ans = ret;
           answer[0] = pos;
           answer[1] = len;
         }
@@ -31,29 +37,24 @@ public class Jewelry {
     }
     return answer;
   }
-
-  public static int init(String[] gems) {
-
+  public static int isCorrect(int start, int end, String[] gems, int least) {
+    int ret = 9999;
     ArrayList<String> arrayList = new ArrayList<String>();
-    for(int i=0; i<gems.length; i++){
-      if(gems[i].length() != 0 && !arrayList.contains(gems[i]))
+    for(int i=start; i<=end; i++) {
+      if(!arrayList.contains(gems[i]))
+        arrayList.add(gems[i]);
+    }   
+    if(arrayList.size() == least) ret = end - start;
+
+    return ret;
+  }
+  public static int init(String[] gems) {
+    ArrayList<String> arrayList = new ArrayList<String>();
+    for(int i=1; i<gems.length; i++){
+      if(!arrayList.contains(gems[i]))
         arrayList.add(gems[i]);
     }
     return arrayList.size();
-  }
-
-  public static int isCorrect(int start, int end, String[] gems, int least) {
-
-    int ret = -1;
-    ArrayList<String> arrayList = new ArrayList<String>();
-    for(int i=start; i<=end; i++) {
-      if(gems[i].length() != 0 && !arrayList.contains(gems[i]))
-        arrayList.add(gems[i]);
-    }   
-    if(arrayList.size() == least) {
-      ret = end - start;
-    }
-    return ret;
   }
 
 }
