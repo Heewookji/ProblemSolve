c86819974891168bc3f3f817793c710d9d638a26
Heewookjigit <wlgmldnr@daum.net>
2020-06-24 17:59:06 +0900

    jlis

--- src/main/java/p13jlis/Main.java
+++ src/main/java/p13jlis/Main.java
@@ -1,65 +1,56 @@
 package p13jlis;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-  
+
   static int[] A;
   static int[] B;
   static int n;
   static int m;
   static int[][] cache = new int[101][101];
   static final long MIN = Long.MIN_VALUE;
-  
-  public static void main(String[] args) throws IOException {
-    
-    StringBuilder sb = new StringBuilder();
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
-    int testN = Integer.parseInt(br.readLine());
-    for(int t=0; t<testN; t++) {
-      
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      n = Integer.parseInt(st.nextToken());
-      m = Integer.parseInt(st.nextToken());
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int testN = sc.nextInt();
+
+    for (int t = 0; t < testN; t++) {
+
+      n = sc.nextInt();
+      m = sc.nextInt();
       A = new int[n];
-      st = new StringTokenizer(br.readLine());
-      for(int i=0; i<n; i++) {
-        A[i] = Integer.parseInt(st.nextToken());
-      }
+      for (int i = 0; i < n; i++)
+        A[i] = sc.nextInt();
       B = new int[m];
-      st = new StringTokenizer(br.readLine());
-      for(int i=0; i<m; i++) {
-        B[i] = Integer.parseInt(st.nextToken());
-      }
-      for(int i=0; i<101; i++) {
+      for (int i = 0; i < m; i++)
+        B[i] = sc.nextInt();
+      for (int i = 0; i < 101; i++)
         Arrays.fill(cache[i], -1);
-      }
-      sb.append(find(-1,-1) - 2).append("\n");
+
+      System.out.println(find(-1, -1) - 2);
     }
-    System.out.print(sb.toString());
   }
-  
+
   private static int find(int indexA, int indexB) {
-    
-    int ret = cache[indexA+1][indexB+1];
-    if (ret != -1)
-        return ret;
-    
-    ret = 2;
-    long a = (indexA == -1? MIN : A[indexA]);
-    long b = (indexB == -1? MIN : B[indexB]);
+
+    if (cache[indexA + 1][indexB + 1] != -1)
+      return cache[indexA + 1][indexB + 1];
+
+    int ret = 2;
+    long a = (indexA == -1 ? MIN : A[indexA]);
+    long b = (indexB == -1 ? MIN : B[indexB]);
     long maxElement = Math.max(a, b);
-    for(int nextA=indexA+1; nextA<n; nextA++) {
-      if(maxElement < A[nextA]) ret = Math.max(ret, find(nextA, indexB) + 1);
+    for (int nextA = indexA + 1; nextA < n; nextA++) {
+      if (maxElement < A[nextA])
+        ret = Math.max(ret, find(nextA, indexB) + 1);
     }
-    for(int nextB=indexB+1; nextB<m; nextB++) {
-      if(maxElement < B[nextB]) ret = Math.max(ret, find(indexA, nextB) + 1);
+    for (int nextB = indexB + 1; nextB < m; nextB++) {
+      if (maxElement < B[nextB])
+        ret = Math.max(ret, find(indexA, nextB) + 1);
     }
-    return cache[indexA+1][indexB+1] = ret;
+    return cache[indexA + 1][indexB + 1] = ret;
   }
 }
