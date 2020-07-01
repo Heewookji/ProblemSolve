0b4bbc5ab27cb34717c82a372475551205290050
heewookji <wlgmldnr@daum.net>
2020-07-01 22:40:33 +0900

    numb3rs

--- src/main/java/p21numb3rs/Main2.java
+++ src/main/java/p21numb3rs/Main2.java
@@ -16,37 +16,38 @@ public class Main2 {
       int day = sc.nextInt();
       int jail = sc.nextInt();
       int[][] link = new int[n][n];
+      int[] deg = new int[n];
 
-      for (int y = 0; y < n; y++)
-        for (int x = 0; x < n; x++)
+      for (int y = 0; y < n; y++) {
+        int count = 0;
+        for (int x = 0; x < n; x++) {
           link[y][x] = sc.nextInt();
-      int retN = sc.nextInt();
-      double[][] cache = new double[day + 1][n];
-
-      for (int r = 0; r < retN; r++) {
-        for (int c = 0; c < cache.length; c++)
-          Arrays.fill(cache[c], -1);
-        System.out.println(recursive(day, jail, link, sc.nextInt(), cache));
+          if (link[y][x] == 1)
+            count++;
+        }
+        deg[y] = count;
       }
 
+      int retN = sc.nextInt();
+      double[][] cache = new double[day + 1][n];
+      for (int c = 0; c < cache.length; c++)
+        Arrays.fill(cache[c], -1);
+      for (int r = 0; r < retN; r++)
+        System.out.println(recursive(day, sc.nextInt(), link, jail, cache, deg));
     }
   }
 
-  public static double recursive(int day, int now, final int[][] link, int target, double[][] cache) {
+  public static double recursive(int day, int now, final int[][] link, int target, double[][] cache, int[] deg) {
 
     if (day == 0)
       return now == target ? 1 : 0;
     if (cache[day][now] != -1)
       return cache[day][now];
+
     double ret = 0;
-    double count = 0;
-    for (int next = 0; next < link.length; next++) {
-      if (link[now][next] == 1) {
-        count++;
-        ret += recursive(day - 1, next, link, target, cache);
-      }
-    }
-    ret *= 1 / count;
+    for (int past = 0; past < link.length; past++)
+      if (link[now][past] == 1)
+        ret += recursive(day - 1, past, link, target, cache, deg) / deg[past];
 
     return cache[day][now] = ret;
   }
