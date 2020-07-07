0f1777c93692b775ddc828b72b07402347a09bd1
Heewookjigit <wlgmldnr@daum.net>
2020-07-07 17:53:21 +0900

    traversal

--- src/main/java/p25traversal/Main.java
+++ src/main/java/p25traversal/Main.java
@@ -1,4 +1,18 @@
 package p25traversal;
 
+import java.util.Scanner;
+
 public class Main {
+
+  public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+    int testN = sc.nextInt();
+
+    for(int i = 0;  i < testN; i++){
+
+
+      
+    }
+  }
 }
