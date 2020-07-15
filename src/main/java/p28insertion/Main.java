e85761d2deb392ce5bbbf98395f7cc65028ebe9e
heewookji <wlgmldnr@daum.net>
2020-07-15 22:25:07 +0900

    insertion

---
+++ src/main/java/p28insertion/Main.java
@@ -0,0 +1,18 @@
+package p28insertion;
+
+import java.util.Scanner;
+
+public class Main {
+  
+  public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+    int testN = sc.nextInt();
+
+    for(int i = 0; i < testN; i++){
+      int n = sc.nextInt();
+    }
+
+
+  }
+}
