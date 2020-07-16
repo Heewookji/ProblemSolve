f9cdf78d7448fb6d14d9ae917bf1b98f8addbdb8
Heewookjigit <wlgmldnr@daum.net>
2020-07-16 21:32:21 +0900

    asymtiling

--- src/main/java/p28insertion/Main.java
+++ src/main/java/p28insertion/Main.java
@@ -11,8 +11,23 @@ public class Main {
 
     for(int i = 0; i < testN; i++){
       int n = sc.nextInt();
+      int[] resultArr = new int[n];
+      int[] originArr = new int[n];
+      for(int j = 0; j < n; j++)
+       resultArr[j] = sc.nextInt();
+      find(n, originArr, resultArr);
+      for(int j = 0; j < n; j++)
+        System.out.print(originArr[j] + " ");
+      System.out.println();
     }
+  }
 
+  private static void find(int n, int[] originArr, final int[] resultArr){
+    
+    for(int i = n - 1; i >= 0; i--){
+      int larger = resultArr[i];
+      originArr[i] = n + 1 - larger;
+    }
 
   }
 }
