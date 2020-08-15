a9497f29a6c03264c5693c0e598bb15a2f3a7fd5
heewookji <wlgmldnr@daum.net>
2020-08-15 21:16:45 +0900

    sortingGame

--- src/p34sortgame/Main.java
+++ src/p34sortgame/Main.java
@@ -1,11 +1,41 @@
 package p34sortgame;
 
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
 import java.util.Scanner;
+
+class ArrayWrapper {
+
+  private int[] list;
+
+  public ArrayWrapper(int[] list) {
+    this.list = list;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    ArrayWrapper another = (ArrayWrapper) obj;
+    return Arrays.equals(this.getList(), another.getList());
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int hashCode = 1;
+    String line = "";
+    for (int i : list)
+      line += String.valueOf(i);
+    hashCode = prime * hashCode + line.hashCode();
+    return hashCode;
+  }
+
+  public int[] getList() {
+    return this.list;
+  }
+
+}
 
 public class Main {
 
@@ -19,39 +49,46 @@ public class Main {
       int[] list = new int[n];
       for (int j = 0; j < n; j++)
         list[j] = scanner.nextInt();
-      System.out.println(bfs(list));
+      System.out.println(bfs(new ArrayWrapper(list), list.length));
     }
     scanner.close();
   }
 
-  private static int bfs(int[] list) {
+  private static int bfs(ArrayWrapper list, int n) {
 
-    int n = list.length;
-    int[] sorted = new int[n];
-    Arrays.sort(sorted);
-    Queue<int[]> que = new LinkedList<>();
-    Map<int[], Integer> distance = new HashMap<>();
+    ArrayWrapper sorted = new ArrayWrapper(list.getList().clone());
+    Arrays.sort(sorted.getList());
+    Queue<ArrayWrapper> que = new LinkedList<>();
+    LinkedHashMap<ArrayWrapper, Integer> distance = new LinkedHashMap<>();
     distance.put(list, 0);
     que.add(list);
 
     while (!que.isEmpty()) {
-      int[] here = que.poll();
+      ArrayWrapper here = que.poll();
       int cost = distance.get(here);
       if (here.equals(sorted))
         return cost;
       for (int i = 0; i < n; i++) {
-        for (int j = i + 2; j < n; j++) {
-          int[] there = null; // reverse(here)
-          if (distance.get(there) == 0) {
+        for (int j = i + 2; j <= n; j++) {
+          ArrayWrapper there = reverse(here, i, j);
+          if (!distance.containsKey(there)) {
             distance.put(there, cost + 1);
             que.add(there);
           }
-          // reverse
         }
       }
     }
     return -1;
+  }
 
+  private static ArrayWrapper reverse(ArrayWrapper array, int start, int end) {
+    int[] newArray = array.getList().clone();
+    for (int point = start; point - start < (end - start) / 2; point++) {
+      int temp = newArray[point];
+      newArray[point] = newArray[end - (point - start) - 1];
+      newArray[end - (point - start) - 1] = temp;
+    }
+    return new ArrayWrapper(newArray);
   }
 
 }
