c8863317cb9cf70122d0c2a1379ca892decbee7e
heewookji <wlgmldnr@daum.net>
2020-08-24 20:42:09 +0900

    feat: childrenday

---
+++ src/common/Number.java
@@ -0,0 +1,34 @@
+package common;
+
+import java.util.Scanner;
+
+// 숫자를 다루는 법
+public class Number {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String method = scanner.nextLine();
+    int ret = 0;
+
+    switch (method) {
+      case "숫자뒤집기":
+        System.out.print("뒤집을숫자 = ");
+        ret = reverseNumber(scanner.nextInt());
+      default:
+        break;
+    }
+
+    System.out.println("결과 = " + ret);
+    scanner.close();
+  }
+
+  private static int reverseNumber(int number) {
+    int result = 0;
+    while (number > 0) {
+      result = result * 10 + number % 10;
+      number /= 10;
+    }
+    return result;
+  }
+
+}
--- src/p35childrenday/Main.java
+++ src/p35childrenday/Main.java
@@ -1,8 +1,33 @@
 package p35childrenday;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+
+class Pair {
+
+  private int parent = -1;
+  private int choice = -1;
+
+  public int getParent() {
+    return this.parent;
+  }
+
+  public int getChoice() {
+    return this.choice;
+  }
+
+  public void setParent(int parent) {
+    this.parent = parent;
+  }
+
+  public void setChoice(int choice) {
+    this.choice = choice;
+  }
+
+}
 
 public class Main {
 
@@ -11,30 +36,59 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     int caseN = scanner.nextInt();
     for (int i = 0; i < caseN; i++) {
-      ArrayList<Integer> numbers = makeNumbers(scanner.nextInt());
+      String numbers = scanner.next();
       int n = scanner.nextInt();
       int m = scanner.nextInt();
-      int ans = solve(numbers, n, m);
-      System.out.println(ans != -1 ? ans : "IMPOSSIBLE");
+      Pair[] graph = makeGraph(numbers, n, m);
+      System.out.println(solve(n, m, graph));
     }
+    scanner.close();
   }
 
-  private static ArrayList<Integer> makeNumbers(int numbers) {
-    ArrayList<Integer> ret = new ArrayList<>();
-    int result = 0;
-    while (numbers > 0) {
-      result = result * 10 + numbers % 10;
-      numbers /= 10;
-    }
-    while (result > 0) {
-      ret.add(result % 10);
-      result /= 10;
+  private static String solve(int n, int m, Pair[] graph) {
+
+    String ret = "";
+    int target = n + m;
+    if (graph[target].getParent() == -1)
+      return "IMPOSSIBLE";
+    while (graph[target].getParent() != target) {
+      ret = (char) (graph[target].getChoice() + '0') + ret;
+      target = graph[target].getParent();
     }
     return ret;
   }
 
-  private static int solve(ArrayList<Integer> numbers, int n, int m) {
-    return -1;
+  private static Pair[] makeGraph(String number, int n, int m) {
+
+    String[] numbers = number.split("");
+    Arrays.sort(numbers);
+    number = String.join("", numbers);
+    Pair[] graph = new Pair[2 * n];
+    for (int i = 0; i < graph.length; i++)
+      graph[i] = new Pair();
+    Queue<Integer> que = new LinkedList<>();
+    graph[0].setParent(0);
+    que.add(0);
+
+    while (!que.isEmpty()) {
+      int here = que.poll();
+      for (int i = 0; i < number.length(); i++) {
+        int there = append(here, number.charAt(i) - '0', n);
+        if (graph[there].getParent() == -1) {
+          graph[there].setParent(here);
+          graph[there].setChoice(number.charAt(i) - '0');
+          que.add(there);
+        }
+      }
+    }
+    return graph;
+  }
+
+  private static int append(int here, int next, int mod) {
+    int there = here * 10 + next;
+    if (there >= mod)
+      return mod + there % mod;
+    return there % mod;
   }
 
 }
