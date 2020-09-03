230cbccf12d9ee9f99353fc86fb526f1beba49b2
heewookji <wlgmldnr@daum.net>
2020-09-03 23:06:21 +0900

    feat: 연속숫자개수수

--- src/common/NumberUtil.java
+++ src/common/NumberUtil.java
@@ -9,13 +9,17 @@ public class NumberUtil {
     Scanner scanner = new Scanner(System.in);
     System.out.print("실행할 작업 = ");
     String method = scanner.nextLine();
-    int origin = 123;
+    System.out.print("숫자 = ");
+    int origin = scanner.nextInt();
     int ret = 0;
-    System.out.println("숫자 = " + origin);
 
     switch (method) {
       case "뒤집기":
         ret = reverse(origin);
+        break;
+      case "연속숫자개수":
+        System.out.print("찾을 숫자 = ");
+        ret = findContinuous(origin, scanner.nextInt());
         break;
       default:
         System.out.println("작업을 지정해주세요");
@@ -31,6 +35,26 @@ public class NumberUtil {
       number /= 10;
     }
     return result;
+  }
+
+  private static int findContinuous(int num, int target) {
+    int ret = 0;
+    int count = 0;
+    int past = 0;
+
+    while (num > 0) {
+      int i = num % 10;
+      if (i == target && (count == 0 || past == target))
+        count++;
+      else {
+        ret = Math.max(ret, count);
+        count = 0;
+      }
+      num /= 10;
+      past = i;
+    }
+
+    return Math.max(ret, count);
   }
 
 }
