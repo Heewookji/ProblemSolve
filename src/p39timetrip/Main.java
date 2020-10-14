ceac79cbe77ea4ee50aa53144565f4ea7a8ee8fb
heewookji <wlgmldnr@daum.net>
2020-10-14 20:39:56 +0900

    feat: timetrip

--- src/p39timetrip/Main.java
+++ src/p39timetrip/Main.java
@@ -4,13 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-import common.algorithm.Floyd;
-import domain.Pair;
-
 public class Main {
 
-  final static int MAX = Integer.MAX_VALUE;
-  final static int MIN = Integer.MIN_VALUE;
+  final static int INF = 987654321;
 
   public static void main(String[] args) {
 
@@ -21,41 +17,42 @@ public class Main {
       int n = scanner.nextInt();
       int w = scanner.nextInt();
       ArrayList<Pair<Integer>>[] adj = new ArrayList[n];
-      int[][] adjForFloyd = new int[n][n];
+      boolean[][] reachable = new boolean[n][n];
       for (int j = 0; j < n; j++)
         adj[j] = new ArrayList<>();
+
       for (int j = 0; j < w; j++) {
         int here = scanner.nextInt();
         int there = scanner.nextInt();
         int cost = scanner.nextInt();
         adj[here].add(new Pair<>(there, cost));
-        adjForFloyd[here][there] = cost;
+        reachable[here][there] = true;
       }
-      int[] upper = new int[n];
-      int[] lower = new int[n];
-      Arrays.fill(upper, MAX);
-      Arrays.fill(lower, MAX);
-      Floyd.solution3(adjForFloyd, n);
 
-      int ret1 = solve(0, 1, adj, upper, adjForFloyd);
+      Floyd.solution3(reachable, n);
+
+      int ret1 = solve(0, 1, adj, reachable);
 
       for (int j = 0; j < n; j++)
         for (Pair<Integer> hole : adj[j])
           hole.setCost(-hole.getCost());
 
-      int ret2 = solve(0, 1, adj, lower, adjForFloyd);
+      int ret2 = solve(0, 1, adj, reachable);
 
-      if (ret1 == MAX)
+      if (ret1 == INF)
         System.out.println("UNREACHABLE");
       else
-        System.out.println((ret1 == MIN ? "INFINITY" : ret1) + " " + (ret2 == MIN ? "INFINITY" : -ret2));
+        System.out.println((ret1 == -INF ? "INFINITY" : ret1) + " " + (ret2 == -INF ? "INFINITY" : -ret2));
     }
   }
 
-  private static int solve(final int start, final int target, final ArrayList<Pair<Integer>>[] adj, int[] upper,
-      int[][] reachable) {
+  private static int solve(final int start, final int target, final ArrayList<Pair<Integer>>[] adj,
+      final boolean[][] reachable) {
+
+    int[] upper = new int[adj.length];
+    Arrays.fill(upper, INF);
     upper[start] = 0;
-    for (int iter = 0; iter < adj.length; iter++)
+    for (int iter = 0; iter < adj.length - 1; iter++)
       for (int here = 0; here < adj.length; here++)
         for (int i = 0; i < adj[here].size(); i++) {
           int there = adj[here].get(i).getNum();
@@ -67,12 +64,53 @@ public class Main {
         int there = adj[here].get(i).getNum();
         int cost = adj[here].get(i).getCost();
         if (upper[there] > upper[here] + cost) {
-          if (reachable[start][here] < Floyd.MAX && reachable[here][target] < Floyd.MAX)
-            return MIN;
+          if (reachable[start][here] && reachable[here][target])
+            return -INF;
         }
       }
     }
+    if (!reachable[start][target] && upper[target] != INF)
+      return INF;
     return upper[target];
   }
+}
 
+class Floyd {
+
+  public static boolean[][] solution3(boolean[][] reachable, int n) {
+    for (int i = 0; i < n; i++)
+      reachable[i][i] = true;
+    for (int k = 0; k < n; k++)
+      for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+          reachable[i][j] |= reachable[i][k] && reachable[k][j];
+    return reachable;
+  }
+}
+
+class Pair<E extends Comparable<E>> implements Comparable<Pair<E>> {
+  private int num;
+  private E cost;
+
+  public Pair(int num, E cost) {
+    this.num = num;
+    this.cost = cost;
+  }
+
+  public E getCost() {
+    return cost;
+  }
+
+  public int getNum() {
+    return num;
+  }
+
+  public void setCost(E cost) {
+    this.cost = cost;
+  }
+
+  @Override
+  public int compareTo(Pair<E> another) {
+    return this.cost.compareTo(another.cost);
+  }
 }
