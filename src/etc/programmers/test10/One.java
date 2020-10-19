1bd2e05e649f55f64ec64b62f1f492c736a25508
heewookji <wlgmldnr@daum.net>
2020-10-19 18:45:34 +0900

    feat: programmers10 One

--- src/etc/programmers/test10/One.java
+++ src/etc/programmers/test10/One.java
@@ -1,6 +1,8 @@
 package etc.programmers.test10;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
 
 class One {
   public int solution(int n) {
@@ -16,5 +18,20 @@ class One {
       n *= 3;
     }
     return sum;
+  }
+
+  public int solution2(int n) {
+    int ret = 0;
+    Stack<Integer> stack = new Stack<>();
+    while (n != 0) {
+      stack.add(n % 3);
+      n /= 3;
+    }
+    Collections.reverse(stack);
+    while (!stack.isEmpty()) {
+      ret *= 3;
+      ret += stack.pop();
+    }
+    return ret;
   }
 }
