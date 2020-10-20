cd3123d79f26182ea19f8666f047d7862da7a67d
heewookji <wlgmldnr@daum.net>
2020-10-20 18:18:56 +0900

    feat: programmers10 Two

--- src/etc/programmers/test10/Two.java
+++ src/etc/programmers/test10/Two.java
@@ -1,50 +1,29 @@
 package etc.programmers.test10;
 
 public class Two {
+  int[] ret = new int[2];
 
   public int[] solution(int[][] arr) {
     int n = arr.length;
-    return recursive(arr, 0, 0, n, n);
-  }
-
-  private int[] recursive(int[][] arr, int startX, int startY, int endX, int endY) {
-    int[] ret = new int[2];
-    if (endX - startX == 1) {
-      ret[arr[startY][startX]] = 1;
-      return ret;
-    }
-    boolean flag0 = false;
-    boolean flag1 = false;
-    for (int y = startY; y < endY; y++) {
-      for (int x = startX; x < endX; x++) {
-        if (arr[y][x] == 0)
-          flag0 = true;
-        else
-          flag1 = true;
-      }
-    }
-    if (!(flag1 && flag0)) {
-      if (flag0)
-        ret[0] = 1;
-      if (flag1)
-        ret[1] = 1;
-      return ret;
-    }
-    ;
-    int half = (endY - startY) / 2;
-    int[] another = recursive(arr, startX, startY, startX + half, startY + half);
-    ret[0] += another[0];
-    ret[1] += another[1];
-    another = recursive(arr, startX, startY + half, startX + half, endY);
-    ret[0] += another[0];
-    ret[1] += another[1];
-    another = recursive(arr, startX + half, startY, endX, startY + half);
-    ret[0] += another[0];
-    ret[1] += another[1];
-    another = recursive(arr, startX + half, startY + half, endX, endY);
-    ret[0] += another[0];
-    ret[1] += another[1];
-
+    recursive(arr, 0, 0, n);
     return ret;
+  }
+  private int isAllSame(final int[][] arr, int startX, int startY, int n) {
+    for (int y = startY; y < startY + n; y++)
+      for (int x = startX; x < startX + n; x++)
+        if (arr[startY][startX] != arr[y][x])
+          return -1;
+    return arr[startY][startX];
+  }
+  private void recursive(int[][] arr, int startX, int startY, int n) {
+    int check = isAllSame(arr, startX, startY, n);
+    if (check != -1) {
+      ret[check]++;
+      return;
+    }
+    n /= 2;
+    for (int i = 0; i < 2; i++)
+      for (int j = 0; j < 2; j++)
+        recursive(arr, startX + (i * n), startY + (j * n), n);
   }
 }
