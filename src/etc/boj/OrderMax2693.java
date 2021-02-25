3da862d3accde6a94143d2e7457b87301e1cedaf
heewookji <wlgmldnr@daum.net>
2021-02-25 14:28:01 +0900

    feat: 2693

---
+++ src/etc/boj/OrderMax2693.java
@@ -0,0 +1,24 @@
+package etc.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class OrderMax2693 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(br.readLine());
+    while (T-- > 0) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      ArrayList<Integer> list = new ArrayList<>();
+      while (st.hasMoreTokens())
+        list.add(Integer.parseInt(st.nextToken()));
+      Collections.sort(list);
+      System.out.println(list.get(7));
+    }
+  }
+}
